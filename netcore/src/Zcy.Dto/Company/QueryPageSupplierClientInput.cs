2b616ae4483c0e6d12313af347df6197192bba74
成 <137651076@qq.com>
2024-07-11 22:34:53 +0800

    bug fix

--- netcore/src/Zcy.Dto/Company/QueryPageSupplierClientInput.cs
+++ netcore/src/Zcy.Dto/Company/QueryPageSupplierClientInput.cs
@@ -14,5 +14,11 @@ namespace Zcy.Dto.Company
         [ZcyQuery(nameof(SupplierClient.PhoneNumber), ZcyOperator.Like)]
         [ZcyQuery(nameof(SupplierClient.ClientName), ZcyOperator.Like)]
         public string? KeyWord { get; set; }
+
+        /// <summary>
+        /// 客户类型
+        /// </summary>
+        [ZcyQuery(nameof(SupplierClient.ClientType), ZcyOperator.Equal)]
+        public ClientTypeEnum? ClientType { get; set; }
     }
 }
